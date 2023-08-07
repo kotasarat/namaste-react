b7590a9af9b843eaef24ca53f653ba1b554cb163
kotasarat <kota.sarat@gmail.com>
2023-08-07 16:29:56 +0530

    Ep13 Test case for clear button

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -62,3 +62,36 @@ it("Should search ResList for top rated restaurants", async () => {
 
   expect(resCardsAfterSearch.length).toBe(4);
 });
+
+it("Should clear all top rated restaurants and show complete ResList", async () => {
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>
+    )
+  );
+
+  const resCardsBeforeSearch = screen.getAllByTestId("resCard");
+
+  expect(resCardsBeforeSearch.length).toBe(9);
+
+  const topRatedResListButton = screen.getByRole("button", {
+    name: "Top Rated Restaurants",
+  });
+
+  fireEvent.click(topRatedResListButton);
+
+  const resCardsAfterSearch = screen.getAllByTestId("resCard");
+
+  expect(resCardsAfterSearch.length).toBe(4);
+
+  const clearButton = screen.getByRole("button", {
+    name: "Clear",
+  });
+
+  fireEvent.click(clearButton);
+
+  const resCardsAfterClear = screen.getAllByTestId("resCard");
+  expect(resCardsAfterClear.length).toBe(9);
+});
